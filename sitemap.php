8807f2b3bde4f1a127c17ee2005ad9ac096951f0
thboehi <thomaboehi@gmail.com>
2025-01-08 11:59:24 +0100

    Added sitemap

---
+++ sitemap.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/xml');
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+$pages = [
+    ['loc' => 'https://www.drgracebianchi.ch/', 'lastmod' => '2025-01-08', 'priority' => '1.0'],
+    ['loc' => 'https://www.drgracebianchi.ch/about', 'lastmod' => '2025-01-08', 'priority' => '0.8'],
+    ['loc' => 'https://www.drgracebianchi.ch/contact', 'lastmod' => '2025-01-08', 'priority' => '0.8'],
+];
+foreach ($pages as $page) {
+    echo '<url>';
+    echo '<loc>' . $page['loc'] . '</loc>';
+    echo '<lastmod>' . $page['lastmod'] . '</lastmod>';
+    echo '<priority>' . $page['priority'] . '</priority>';
+    echo '</url>';
+}
+echo '</urlset>';
+?>
