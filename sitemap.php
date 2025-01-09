4ee6b3f100056b903e44508bc17902957bf86bb8
thboehi <thomaboehi@gmail.com>
2025-01-09 12:07:33 +0100

    Updated sitemap

--- sitemap.php
+++ sitemap.php
@@ -4,9 +4,9 @@ header('Content-Type: application/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 $pages = [
-    ['loc' => 'https://www.drbianchi.ch/', 'lastmod' => '2025-01-08', 'priority' => '1.0'],
-    ['loc' => 'https://www.drbianchi.ch/about', 'lastmod' => '2025-01-08', 'priority' => '0.8'],
-    ['loc' => 'https://www.drbianchi.ch/contact', 'lastmod' => '2025-01-08', 'priority' => '0.8'],
+    ['loc' => 'https://www.drbianchi.ch/', 'lastmod' => '2025-01-09', 'priority' => '1.0'],
+    ['loc' => 'https://www.drbianchi.ch/about', 'lastmod' => '2025-01-09', 'priority' => '0.8'],
+    ['loc' => 'https://www.drbianchi.ch/contact', 'lastmod' => '2025-01-09', 'priority' => '0.8'],
 ];
 foreach ($pages as $page) {
     echo '<url>';
