98760483f25a55bfedd9645e79805e73f7313da7
thboehi <thomaboehi@gmail.com>
2025-02-20 11:41:43 +0100

    First beta commit

--- components/header.php
+++ components/header.php
@@ -23,10 +23,7 @@
         <!-- Google Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Della+Respira&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wdth,wght,YTLC@0,6..12,75..125,200..1000,440..540;1,6..12,75..125,200..1000,440..540&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
 
         <!--- Tippy.js --->
         <script src="https://unpkg.com/@popperjs/core@2"></script>
--- components/hero.php
+++ components/hero.php
@@ -1,8 +1,9 @@
 <section id="hero" class="hero padding">
     <div class="hero-text-container">
-        <h1>Dre P. Grace Bianchi Movarekhi</h1>
-        <p>Médecin de la reproduction à Genève</p>
-        <p>Spécialiste FMH - Gyn.-obst., Méd. de la reproduction</p>
+        <h1>Dre P. Grace</br>Bianchi Movarekhi</h1>
+        <h2>Médecin de la reproduction à Genève</h2>
+        <h2>Spécialiste FMH - Gyn.-obst., Méd. de la reproduction</h2>
         <a class="cta" href="/contact"><img src="../favicon.png" alt="Logo">Prendre rendez-vous</a>
     </div>
+    <img class="hero-img" src="./img/team/grace_transparent.png" alt="Dre. Grace Bianchi">
 </section>
